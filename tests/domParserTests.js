899cc09b7e4729ef3886bf53df929ef9039e79ea
Kendrick Schuettler <schuettlerk@google.com>
2019-02-04 15:57:39 -0500

    Test cleanup

--- tests/domParserTests.js
+++ tests/domParserTests.js
@@ -27,25 +27,13 @@ describe('DOM Parser Functions', function() {
     it('Should return an object with the appropriate sets', function checkForNeededSets() {
       const selectors = DomParser.extractDomData(ampFileStub);
       assert.ok(selectors.ampElementSelectors);
-      assert.ok(selectors.attributeSelectors);
-      assert.ok(selectors.classSelectors);
-      assert.ok(selectors.elementSelectors);
-      assert.ok(selectors.emptySelectors);
-      assert.ok(selectors.idSelectors);
-      assert.ok(selectors.nestedSelectors);
-    });
-
-    /** Should only return true if input file has at least one of each selector type */
-    it('Should return an object with non-empty sets if appropriate selectors exist', function checkForNonEmptySets() {
-      const selectors = DomParser.extractDomData(ampFileStub);
-
-      assert.ok(selectors.ampElementSelectors.size > 0);
-      assert.ok(selectors.attributeSelectors.size > 0);
-      assert.ok(selectors.classSelectors.size > 0);
-      assert.ok(selectors.elementSelectors.size > 0);
-      // assert.ok(selectors.emptySelectors.size > 0); not currently utilized
-      assert.ok(selectors.idSelectors.size > 0);
-      assert.ok(selectors.nestedSelectors.size > 0);
+      assert.ok(selectors.polyFills);
+      assert.ok(selectors.escapedPseudos);
+      assert.ok(selectors.pseudos);
+      assert.ok(selectors.general);
+      assert.ok(selectors.commaSeparatedSelectors);
+      assert.ok(selectors.ampElementSelectors);
+      assert.ok(selectors.joinedRawCss);
     });
   })
 });
