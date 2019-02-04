cad35b682c7a1159d48b00e88d602e5dbdec851f
Kendrick Schuettler <schuettlerk@google.com>
2019-02-04 14:45:16 -0500

    Added a bunch of T0 tests and change the way css shaking is handled.

--- tests/typeZeroOptimizerTests.js
+++ tests/typeZeroOptimizerTests.js
@@ -1,41 +1,74 @@
 const assert = require('assert');
+const path = require('path');
 const AmpFile = require('../lib/main/AmpFile');
-const inputHtml = 'tests/selectors/input.html';
 const fs = require('fs');
 const typeZeroOptimizations = require('../lib/main/typeZeroOptimizations');
+
+const inputHtml = 'tests/selectors/input.html';
+
+
+const expected = fs.readdirSync(path.join(__dirname, 'selectors/expected')),
+    unused = fs.readdirSync(path.join(__dirname, 'selectors/unused'));
+
+
+const tests = expected.reduce((acc, testType) => {
+  acc[testType] = {
+    expected: true,
+    unused: false,
+  };
+  return acc;
+}, {});
+
+unused.forEach(unusedTestType => {
+  if (tests[unusedTestType]) {
+    tests[unusedTestType].unused = true;
+  } else {
+    tests[unusedTestType] = {
+      expected: false,
+      unused: true
+    }
+  }
+});
+
+let type0Html = false;
+
+describe('Type 0 Optimizer Functions', function() {
+  before(done => {
+    const ampFile = new AmpFile(inputHtml);
+    const resultingHtml = typeZeroOptimizations
+        .optimize(ampFile)
+        .prepData()
+        .optimizedHtml
+        .replace(/\n/g,'')
+        .replace(/\s*/g,'');
+
+    if(resultingHtml) {
+      type0Html = resultingHtml;
+    }
+    done()
+  });
+
+
+  Object.keys(tests).forEach(test => {
+
+    if(tests[test].expected) {
+      it(`Should include expected ${test} selectors in CSS`, () => {
+        assert.ok(type0Html.includes(rfs(path.join(__dirname,`selectors/expected/${test}`))));
+      });
+    }
+
+    if(tests[test].unused) {
+      it(`Should not include unused ${test} selectors in CSS`, () => {
+        assert.ok(!type0Html.includes(rfs(path.join(__dirname,`selectors/unused/${test}`))));
+      });
+    }
+  });
+});
 
 /** fs.readFileSync sugar */
 function rfs(file) {
   return fs.readFileSync(file, 'utf-8')
-      .replace(/="{2}/g, "")
       .replace(/\r\n/g, '\n')
-      .replace(/\n/g,"");
+      .replace(/\n/g,"")
+      .replace(/\s*/g,'');
 }
-
-describe('Type 0 Optimizer Functions', function() {
-  describe('#_removeUnusedAdjacentSelectors', function() {});
-  describe('#_removeUnusedAmpSelectors', function() {});
-  describe('#_removeUnusedAttributeSelectors', function() {});
-  describe('#_removeUnusedChildSelectors', function() {});
-  describe('#_removeUnusedClassSelectors', function() {});
-  describe('#_removeUnusedComplexSelectors', function() {});
-  describe('#_removeUnusedElementSelectors', function() {});
-  describe('#_removeEmptySelectors', function() {
-    const ampFile = new AmpFile(inputHtml);
-    const expectedHtml = rfs('tests/selectors/empty/expected.html');
-
-    typeZeroOptimizations.optimize(ampFile);
-
-    assert.strictEqual(expectedHtml, ampFile.prepData().optimizedHtml);
-  });
-  describe('#_removeUnusedEscapedPseudoSelectors', function() {});
-  describe('#_removeUnusedGeneralSiblingSelectors', function() {});
-  describe('#_removeUnusedIdSelectors', function() {});
-  describe('#_removeUnusedNotSelectors', function() {});
-  describe('#_removeUnusedNthChildSelectors', function() {});
-  describe('#_removeUnusedNthOfTypeSelectors', function() {});
-  describe('#_removeUnusedPseudoSelectors', function() {});
-  describe('#_removeUnusedPseudoClassWithPseudoElementSelectors', function() {});
-  describe('#_removeUnusedTargetSelectors', function() {});
-  describe('#_removeUnusedVendorSelectors', function() {});
-});
