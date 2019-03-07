840724899d8ee7913a216751756c3b0e92c2847b
Kendrick Schuettler <schuettlerk@google.com>
2019-03-07 16:47:00 -0500

    Outlined Puppeteer interface and started on Cheerio interface

---
+++ tests/cheerioInterfaceTests.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const fs = require('fs');
+const cheerioInt = require('../lib/interfaces/CheerioInterface');
+
+const fileLoc = './tests/selectors/input.html';
+const html = fs.readFileSync(fileLoc, 'utf-8');
+
+
+let pageRep = false;
+
+describe('CheerioInterface', async () => {
+
+  before(async () => {
+    pageRep = await new cheerioInt(html).init()
+  });
+
+  after(async () => {
+    pageRep.shutdown();
+  });
+
+  it('should execute .init() the first time', async () => {
+    assert.ok(pageRep.page);
+  });
+  it('should shortcut .init() the second time', async () => {
+    const page1 = pageRep.page;
+    const page2  = await pageRep.init();
+    assert.ok(page1 === page2);
+  });
+  it('should count the correct number of "span"', async () => {
+    let count = await pageRep.count('span');
+    assert.strictEqual(count, 3)
+  });
+  it('should count the correct number of ".special-span"', async () => {
+    let count = await pageRep.count('.special-span');
+    assert.strictEqual(count, 2)
+  });
+  it('should count the correct number of "#extra-special-span"', async () => {
+    let count = await pageRep.count('#extra-special-span');
+    assert.strictEqual(count, 1)
+  });
+  it('should count the correct number of ".unused"', async () => {
+    let count = await pageRep.count('.unused');
+    assert.strictEqual(count, 0)
+  });
+  it('should update the amp-img tag dom structure correctly', async ()=>{});
+});
