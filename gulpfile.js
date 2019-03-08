ea95772358f1b04cc0b974b32b75433fc5741462
Kendrick Schuettler <schuettlerk@google.com>
2019-03-08 14:01:26 -0500

    Change UnCSS execution to require the construction ('new AmpUncss(opts)') + asynchronous run ('instance.run(files)') style to allow for async code (Puppeteer execution). Updated gulp and commander files to reflect change.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const ampUnCss = require('./index_test');
 
-gulp.task('default', function(){
+gulp.task('default', async function(){
   return gulp.src('tests/selectors/*.html')
       .pipe(ampUnCss({reportDirectory:'output'}))
       .pipe(gulp.dest('output/'));
--- index_test.js
+++ index_test.js
@@ -4,20 +4,21 @@ const unCss= require('./lib/main/UnCss.js');
 module.exports =  function(options) {
   function gulpUnCss(vinyl, enc, cb) {
     if (vinyl.isBuffer()) {
-      let {optimizedHtmlString, reporting} = unCss(
-          vinyl,
-          Object.assign(options,
-              {
-                streamable: true,
-                report: !!options.report || !!options.reportDirectory || !!options.reportName
-              }
-          ),
-          cb);
-
-      vinyl.contents = Buffer.from(optimizedHtmlString);
+      const processOpts = Object.assign(
+          options,{
+            streamable: true,
+            report: !!options.report || !!options.reportDirectory || !!options.reportName
+      });
+      unCss(processOpts).run(vinyl)
+          .then(results => {
+            let {optimizedHtmlString, reporting} = results;
+            vinyl.contents = Buffer.from(optimizedHtmlString);
+            cb(null, vinyl);
+          })
+    } else {
+      cb(null, vinyl);
     }
 
-    cb(null, vinyl);
   }
   return through.obj(gulpUnCss)
 };
