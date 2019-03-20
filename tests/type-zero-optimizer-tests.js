e4d23df16921536907439dcbc156ab0119093d39
Kendrick Schuettler <schuettlerk@google.com>
2019-03-20 16:55:04 -0400

    Refactored tests/type-zero-optimizer-tests.js in order to adhere to google style guide.

--- tests/type-zero-optimizer-tests.js
+++ tests/type-zero-optimizer-tests.js
@@ -1,14 +1,16 @@
 const assert = require('assert');
-const path = require('path');
-const AmpFile = require('../lib/main/amp-file');
 const fs = require('fs');
-const typeZeroOptimizations = require('../lib/main/type-zero-optimizations');
+const path = require('path');
+
+const AmpFile = require('../lib/main/amp-file.js');
+const typeZeroOptimizations = require('../lib/main/type-zero-optimizations.js');
 
 const inputHtml = 'tests/selectors/static/staticDom.html';
 
 
-const expected = fs.readdirSync(path.join(__dirname, 'selectors/static/expected')),
-    unused = fs.readdirSync(path.join(__dirname, 'selectors/static/unused'));
+const expected = fs.readdirSync(
+    path.join(__dirname, 'selectors/static/expected'));
+const unused = fs.readdirSync(path.join(__dirname, 'selectors/static/unused'));
 
 
 const tests = expected.reduce((acc, testType) => {
@@ -19,14 +21,14 @@ const tests = expected.reduce((acc, testType) => {
   return acc;
 }, {});
 
-unused.forEach(unusedTestType => {
+unused.forEach((unusedTestType) => {
   if (tests[unusedTestType]) {
     tests[unusedTestType].unused = true;
   } else {
     tests[unusedTestType] = {
       expected: false,
-      unused: true
-    }
+      unused: true,
+    };
   }
 });
 
@@ -35,54 +37,56 @@ let type0Html = false;
 describe('Type 0 Optimizer Functions', function() {
   before(async () => {
     const defaultOptions = {
-      optimizationLevel : 0,
+      optimizationLevel: 0,
       streamable: false,
       reportName: 'amp_uncss_report.json',
       reportDirectory: 'reports',
       targetDirectory: './dist',
       filenameDecorator: null,
-      report: false
+      report: false,
     };
-    const ampFile = await new AmpFile(inputHtml, defaultOptions).prep();
+    const ampFile = await new AmpFile(inputHtml, defaultOptions, null).prep();
     await typeZeroOptimizations
         .optimize(ampFile)
-        .rewriteHtmlWithNewCss(defaultOptions);
+        .rewriteHtmlWithNewCss();
     const resultingHtml = ampFile
         .optimizedHtml
-        .replace(/\n/g,'')
-        .replace(/\s*/g,'');
+        .replace(/\n/g, '')
+        .replace(/\s*/g, '');
 
-    if(resultingHtml) {
+    if (resultingHtml) {
       type0Html = resultingHtml;
     }
   });
 
 
-  Object.keys(tests).forEach(test => {
-
-    if(tests[test].expected) {
+  Object.keys(tests).forEach((test) => {
+    if (tests[test].expected) {
       it(`Should include expected ${test} selectors in CSS`, () => {
-        assert.ok(type0Html.includes(rfs(path.join(__dirname,`selectors/static/expected/${test}`))));
+        assert.ok(type0Html.includes(
+            rfs(path.join(__dirname, `selectors/static/expected/${test}`))));
       });
     }
 
-    if(tests[test].unused) {
+    if (tests[test].unused) {
       it(`Should not include unused ${test} selectors in CSS`, () => {
-
-        rfs(path.join(__dirname,`selectors/static/unused/${test}`))
+        rfs(path.join(__dirname, `selectors/static/unused/${test}`))
             .split('/*Separator*/')
-            .forEach(block => {
-              assert.ok(!type0Html.includes(block))
+            .forEach((block) => {
+              assert.ok(!type0Html.includes(block));
             });
       });
     }
   });
 });
 
-/** fs.readFileSync sugar */
-function rfs(file) {
-  return fs.readFileSync(file, 'utf-8')
+/** fs.readFileSync sugar
+ * @param {string} filePath
+ * @return {string}
+ */
+function rfs(filePath) {
+  return fs.readFileSync(filePath, 'utf-8')
       .replace(/\r\n/g, '\n')
-      .replace(/\n/g,"")
-      .replace(/\s*/g,'');
+      .replace(/\n/g, '')
+      .replace(/\s*/g, '');
 }
